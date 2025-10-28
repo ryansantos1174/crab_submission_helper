b2f96453def698f6b528e98093d53a78a2e6d4aa
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-10-28 14:00:55 -0400

    Update crab_template formatting
    Reformat crab_template so that a single file can be used for the NLayers
    selectio, the base selection, and recovery tasks

--- data/templates/crab_template.py
+++ data/templates/crab_template.py
@@ -1,33 +1,34 @@
+
 #!/usr/bin/env python3
 import os
 from CRABClient.UserUtilities import config
 config = config()
 
-request_name = '__REQUEST_NAME__'
+NLayers= "__NLayers__"
+request_name = "__REQUEST_NAME__"
 config.General.requestName = request_name
-config.General.workArea = 'crab'
+config.General.workArea = "crab"
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 
-config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'config_cfg.py'
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "config_cfg.py"
 config.JobType.allowUndistributedCMSSW = True
 
 
-config.Data.inputDataset = "__DATASET__"
+if not NLayers:
+    config.Data.inputDataset = "__DATASET__"
+    config.Data.lumiMask = "__LUMIMASK__"
+    config.Data.inputDBS = "global"
+    config.Data.splitting = "LumiBased"
+    config.Data.unitsPerJob=10
+else:
+    config.Data.inputDBS = "phys03"
+    config.Data.userInputFiles = open("__SKIM_FILE__").readlines()
+    config.Data.splitting = "FileBased"
+    config.Data.unitsPerJob = 1
 
-if "2022" in request_name:
-    config.Data.lumiMask = "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions22/Cert_Collisions2022_355100_362760_Golden.json"
-elif "2023" in request_name:
-    config.Data.lumiMask = "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions23/Cert_Collisions2023_366442_370790_Golden.json"
-elif "2024" in request_name:
-    config.Data.lumiMask = "https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions24/Cert_Collisions2024_378981_386951_Golden.json"
-    
-
-config.Data.inputDBS = 'global'
-config.Data.unitsPerJob = 20
-config.Data.splitting = 'LumiBased'
 
 
 config.Data.publication = False
@@ -40,16 +41,16 @@ config.Data.outputDatasetTag = request_name
 # Uncomment one of the following pairs
 
 
-config.Data.outLFNDirBase = '/store/group/lpclonglived/DisappTrks/'
-config.Site.storageSite = 'T3_US_FNALLPC'
+config.Data.outLFNDirBase = "/store/group/lpclonglived/DisappTrks/"
+config.Site.storageSite = "T3_US_FNALLPC"
 
-#config.Data.outLFNDirBase = '/store/user/%s/' % (user_name)
-#config.Site.storageSite = 'T2_US_Purdue'
-
-
-#config.Data.outLFNDirBase = '/store/group/phys_exotica/disappearingTracks/'
-#config.Site.storageSite = 'T2_CH_CERN'
+#config.Data.outLFNDirBase = "/store/user/%s/" % (user_name)
+#config.Site.storageSite = "T2_US_Purdue"
 
 
-# config.Data.outLFNDirBase = '/store/user/borzari/'
-# config.Site.storageSite = 'T2_BR_SPRACE'
+#config.Data.outLFNDirBase = "/store/group/phys_exotica/disappearingTracks/"
+#config.Site.storageSite = "T2_CH_CERN"
+
+
+# config.Data.outLFNDirBase = "/store/user/borzari/"
+# config.Site.storageSite = "T2_BR_SPRACE"
