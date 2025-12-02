777f27a6feff79bc114588a851c8dac0ef851077
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-12-02 15:10:31 -0600

    Update scripts to CMSSW_15

--- scripts/status.sh
+++ scripts/status.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 # Setup CMSSW environment
-cd /uscms_data/d3/delossan/CMSSW_13_0_13/src
+cd /uscms_data/d3/delossan/CMSSW_15_0_10/src
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 eval `scramv1 runtime -sh`
 cd -
 echo $(hostname) >> /uscms_data/d3/delossan/identify_yourself.log
-crab_helper status -v -r /uscms_data/d3/delossan/CMSSW_13_0_13/src/DisappTrks/BackgroundEstimation/test/ \
+crab_helper status -v -r /uscms_data/d3/delossan/CMSSW_15_0_10/src/DisappTrks/BackgroundEstimation/test/ \
     -l /uscms_data/d3/delossan/crab_status.log \
-    -d /uscms_data/d3/delossan/CMSSW_13_0_13/src/DisappTrks/BackgroundEstimation/test/crab/ \
+    -d /uscms_data/d3/delossan/CMSSW_15_0_10/src/DisappTrks/BackgroundEstimation/test/crab/ \
     --email --ntfy
