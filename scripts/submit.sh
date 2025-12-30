ff0aa8a2321ac1ad4f192b2b51bfe6c85d598b6b
Ryan De Los Santos <ryansantos1174@gmail.com>
2025-12-30 02:24:40 -0600

    Refactor submission script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,14 +1,24 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+set -euo pipefail
 
-# Setup CMSSW environment
-cd /uscms_data/d3/delossan/CMSSW_15_0_10/src
+CMSSW_VERSION="CMSSW_13_0_13"
+BATCH_FILE="2023_TauTag_batch_submission.yaml"
+LOG_FILE="TauTag_2023.log"
+
+CMSSW_SRC="/uscms_data/d3/delossan/${CMSSW_VERSION}/src"
+TEST_DIR="${CMSSW_SRC}/DisappTrks/BackgroundEstimation/test"
+CRAB_DIR="${TEST_DIR}/crab"
+BATCH_PATH="${CMSSW_SRC}/crab_submission_helper/batch_files/${BATCH_FILE}"
+LOG_PATH="/uscms_data/d3/delossan/${LOG_FILE}"
+
+cd "${CMSSW_SRC}"
+set +u
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-eval `scramv1 runtime -sh`
-cd -
+eval "$(scramv1 runtime -sh)"
+set -u
 
-
-crab_helper submit -r /uscms_data/d3/delossan/CMSSW_15_0_10/src/DisappTrks/BackgroundEstimation/test/ \
-    -l /uscms_data/d3/delossan/crab_submit_muon_fiducial_maps_2025-11-19.log \
-    -d /uscms_data/d3/delossan/CMSSW_15_0_10/src/DisappTrks/BackgroundEstimation/test/crab/ \
-    --batch_file /uscms_data/d3/delossan/CMSSW_15_0_10/src/crab_submission_helper/batch_files/2024_fiducial_maps_batch_submission.yaml \
-    --test
+crab_helper submit -r "${TEST_DIR}" \
+  -l "${LOG_PATH}" \
+  -d "${CRAB_DIR}" \
+  --batch_file "${BATCH_PATH}" \
+  --test
