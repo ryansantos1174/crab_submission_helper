ae2feaf8081ce2ff6b8b1cba3931e4b2f9ad0626
Ryan De Los Santos <ryansantos1174@gmail.com>
2026-01-12 12:22:30 -0600

    Fix add_channels parameters

--- src/crab_submission_helper/data/templates/config_selections_template.py
+++ src/crab_submission_helper/data/templates/config_selections_template.py
@@ -10,15 +10,15 @@ NLayers = __NLAYERS__
 if not NLayers:
     # Fake Track Selections
     if selection == "BasicSelection":
-        add_channels  (process,  [basicSelection],                histSets,  weights,  [],  collMap,  variableProducers,  True)
-        add_channels  (process,  [basicSelectionInvertJetMetPhiCut], histSets,  weights,  [],  collMap,  variableProducers,  True, forceNonEmptySkim=True)
+        add_channels  (process,  [basicSelection],                histSets,  weights,  [],  collMap,  variableProducers,  ignoreSkimmedCollections=True, forceNonEmptySkim=True)
+        add_channels  (process,  [basicSelectionInvertJetMetPhiCut], histSets,  weights,  [],  collMap,  variableProducers,  ignoreSkimmedCollections=True, forceNonEmptySkim=True)
 
     if selection == "DisTrkSelection":
-        add_channels  (process,  [disTrkSelectionNoD0CutNHits3],  histSets,        weights,  [],  collMap,  variableProducers,  False, forceNonEmptySkim=False, ignoreSkimmedCollections = True)
-        add_channels  (process,  [disTrkSelectionSidebandD0CutNHits3],  histSets,        weights,  [],  collMap,  variableProducers,  False, forceNonEmptySkim=False, ignoreSkimmedCollections = True)
-        add_channels  (process,  [disTrkSelectionSidebandD0CutNHits4],  histSets,        weights,  [],  collMap,  variableProducers,  False, forceNonEmptySkim=False, ignoreSkimmedCollections = True)
-        add_channels  (process,  [disTrkSelectionSidebandD0CutNHits5],  histSets,        weights,  [],  collMap,  variableProducers,  False, forceNonEmptySkim=False, ignoreSkimmedCollections = True)
-        add_channels  (process,  [disTrkSelectionSidebandD0CutNHits6],  histSets,        weights,  [],  collMap,  variableProducers,  False, forceNonEmptySkim=False, ignoreSkimmedCollections = True)
+        add_channels  (process,  [disTrkSelectionNoD0CutNHits3],  histSets,        weights,  [],  collMap,  variableProducers, forceNonEmptySkim=True, ignoreSkimmedCollections = True)
+        add_channels  (process,  [disTrkSelectionSidebandD0CutNHits3],  histSets,        weights,  [],  collMap,  variableProducers, forceNonEmptySkim=True, ignoreSkimmedCollections = True)
+        add_channels  (process,  [disTrkSelectionSidebandD0CutNHits4],  histSets,        weights,  [],  collMap,  variableProducers, forceNonEmptySkim=True, ignoreSkimmedCollections = True)
+        add_channels  (process,  [disTrkSelectionSidebandD0CutNHits5],  histSets,        weights,  [],  collMap,  variableProducers, forceNonEmptySkim=True, ignoreSkimmedCollections = True)
+        add_channels  (process,  [disTrkSelectionSidebandD0CutNHits6],  histSets,        weights,  [],  collMap,  variableProducers, forceNonEmptySkim=True, ignoreSkimmedCollections = True)
 
     if selection == "ZtoEE":
         add_channels  (process,  [ZtoEE],                           histSetsElectron,  weightsWithEleSF,  scaleFactorProducersWithElectrons,  collMap,  variableProducers, ignoreSkimmedCollections = True, forceNonEmptySkim=True)
