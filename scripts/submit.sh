3e4e9702a1b35cb7bead75e23bd87448bf5612b2
Ryan De Los Santos <ryansantos1174@gmail.com>
2026-01-13 23:19:48 -0600

    Update for 2024 Tau background nlayers

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -2,15 +2,19 @@
 set -euo pipefail
 
 CMSSW_VERSION="CMSSW_15_0_10"
+<<<<<<< Updated upstream
 BATCH_FILE="2024_FakeTracks_background.yml"
 LOG_FILE="FakeTracks_2024EF.log"
+=======
+BATCH_FILE="2024_Tau_background_NLayers.yml"
+LOG_FILE="TauBackground_NLayers_2024_2026-01-13.log"
+>>>>>>> Stashed changes
 
 CMSSW_SRC="/uscms_data/d3/delossan/${CMSSW_VERSION}/src"
 TEST_DIR="${CMSSW_SRC}/DisappTrks/BackgroundEstimation/test"
 CRAB_DIR="${TEST_DIR}/crab"
 BATCH_PATH="${CMSSW_SRC}/crab_submission_helper/batch_files/${BATCH_FILE}"
 LOG_PATH="/uscms_data/d3/delossan/${LOG_FILE}"
-
 cd "${CMSSW_SRC}"
 set +u
 source /cvmfs/cms.cern.ch/cmsset_default.sh
